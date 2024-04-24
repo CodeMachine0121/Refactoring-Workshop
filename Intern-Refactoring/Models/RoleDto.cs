804700b8841d20dbd0a575a1c0aa589c32ed18db
james.hsueh <james.hsueh@titansoft.com.sg>
2024-04-25 00:13:51 +0800

    refactor: rename

--- Intern-Refactoring/Models/RoleDto.cs
+++ Intern-Refactoring/Models/RoleDto.cs
@@ -5,13 +5,13 @@ public class RoleDto
     public Weapon Weapon { get; set; }
     public int Level { get; set; }
 
-    public RoleDomain ToRoleDomain(Job magician)
+    public RoleDomain ToRoleDomain(Job job)
     {
         return new RoleDomain()
         {
             Level = Level,
             Weapon = Weapon,
-            Job = magician
+            Job = job
         };
     }
 }
