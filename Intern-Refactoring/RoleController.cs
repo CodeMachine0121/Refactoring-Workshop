fe2d4139de5ef9f5e656eb81627c8dae9d70d2fd
james.hsueh <james.hsueh@titansoft.com.sg>
2024-04-25 00:30:39 +0800

    refactor: extract method

--- Intern-Refactoring/RoleController.cs
+++ Intern-Refactoring/RoleController.cs
@@ -7,16 +7,21 @@ public class RoleController(IRoleService roleService) : ControllerBase
 
     public RoleResponse GetRoleBy(RoleRequest request)
     {
-        var domain = roleService.GenerateRoleBy(new RoleDto()
-        {
-            Level = request.Level,
-            Weapon = Enum.TryParse<Weapon>(request.Weapon, out var weapon) ? weapon : Weapon.Unkown
-        });
+        var domain = roleService.GenerateRoleBy(ToRoleDto(request));
 
         return new RoleResponse()
         {
             Status = ApiStatus.Success,
             Data = domain
+        };
+    }
+
+    private static RoleDto ToRoleDto(RoleRequest request)
+    {
+        return new RoleDto()
+        {
+            Level = request.Level,
+            Weapon = Enum.TryParse<Weapon>(request.Weapon, out var weapon) ? weapon : Weapon.Unkown
         };
     }
 }
