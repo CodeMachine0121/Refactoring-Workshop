ef17ea1469977c42ec360e06e6b0c4992ba7df13
james.hsueh <james.hsueh@titansoft.com.sg>
2024-04-25 00:00:02 +0800

    feat: tdd green, get warrior

--- Intern-Refactoring/Services/RoleService.cs
+++ Intern-Refactoring/Services/RoleService.cs
@@ -4,6 +4,16 @@ public class RoleService: IRoleService
 {
     public RoleDomain GenerateRoleBy(RoleDto dto)
     {
+        if (dto.Level >= 10 && dto.Weapon == Weapon.Sword)
+        {
+            return new RoleDomain()
+            {
+                Level = dto.Level,
+                Weapon = dto.Weapon,
+                Job = Job.Warrior
+            };
+        }
+
         return new RoleDomain()
         {
             Level = dto.Level,
