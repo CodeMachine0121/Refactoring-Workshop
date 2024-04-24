8cd2d5aa798e3eca6ac5a2840acd937da7b60247
james.hsueh <james.hsueh@titansoft.com.sg>
2024-04-25 00:31:17 +0800

    refactor: make method non-static push into request model

--- Intern-Refactoring/Models/RoleRequest.cs
+++ Intern-Refactoring/Models/RoleRequest.cs
@@ -4,4 +4,13 @@ public class RoleRequest
 {
     public int Level { get; set; }
     public string Weapon { get; set; }
+
+    public RoleDto ToRoleDto()
+    {
+        return new RoleDto()
+        {
+            Level = Level,
+            Weapon = Enum.TryParse<Weapon>(Weapon, out var weapon) ? weapon : Intern.Weapon.Unkown
+        };
+    }
 }
--- Intern-Refactoring/RoleController.cs
+++ Intern-Refactoring/RoleController.cs
@@ -7,21 +7,12 @@ public class RoleController(IRoleService roleService) : ControllerBase
 
     public RoleResponse GetRoleBy(RoleRequest request)
     {
-        var domain = roleService.GenerateRoleBy(ToRoleDto(request));
+        var domain = roleService.GenerateRoleBy(request.ToRoleDto());
 
         return new RoleResponse()
         {
             Status = ApiStatus.Success,
             Data = domain
-        };
-    }
-
-    private static RoleDto ToRoleDto(RoleRequest request)
-    {
-        return new RoleDto()
-        {
-            Level = request.Level,
-            Weapon = Enum.TryParse<Weapon>(request.Weapon, out var weapon) ? weapon : Weapon.Unkown
         };
     }
 }
