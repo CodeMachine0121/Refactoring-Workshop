1009c17c68b1f4a9844887e49df7ade7990d2e8a
james.hsueh <james.hsueh@titansoft.com.sg>
2024-04-25 00:04:26 +0800

    refactor: extract method

--- Intern-Refactoring/Services/RoleService.cs
+++ Intern-Refactoring/Services/RoleService.cs
@@ -4,31 +4,36 @@ public class RoleService: IRoleService
 {
     public RoleDomain GenerateRoleBy(RoleDto dto)
     {
-        if (dto.Level >= 10 && dto.Weapon == Weapon.Stick)
+        if (IsMatchMagician(dto))
         {
-            return new RoleDomain()
-            {
-                Level = dto.Level,
-                Weapon = dto.Weapon,
-                Job = Job.Magician
-            };
+            return GetRoleDomain(dto, Job.Magician);
         }
         
-        if (dto.Level >= 10 && dto.Weapon == Weapon.Sword)
+        if (IsMatchWarrior(dto))
         {
-            return new RoleDomain()
-            {
-                Level = dto.Level,
-                Weapon = dto.Weapon,
-                Job = Job.Warrior
-            };
+            return GetRoleDomain(dto, Job.Warrior);
         }
 
+        return GetRoleDomain(dto, Job.Beginner);
+    }
+
+    private static bool IsMatchWarrior(RoleDto dto)
+    {
+        return dto.Level >= 10 && dto.Weapon == Weapon.Sword;
+    }
+
+    private static RoleDomain GetRoleDomain(RoleDto dto, Job magician)
+    {
         return new RoleDomain()
         {
             Level = dto.Level,
             Weapon = dto.Weapon,
-            Job = Job.Beginner
+            Job = magician
         };
+    }
+
+    private static bool IsMatchMagician(RoleDto dto)
+    {
+        return dto.Level >= 10 && dto.Weapon == Weapon.Stick;
     }
 }
